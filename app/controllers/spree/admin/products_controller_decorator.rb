dea0d660c9f004fcba7d998bf7366d47655862ad
gingray <gingray.dev@gmail.com>
2018-10-08 13:27:46 +0300

    add controller and styles

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,4 +1,11 @@
 Spree::Admin::ProductsController.class_eval do
-  def import
+  def import_csv
+    puts import_csv_params
+    redirect_to import_admin_products_path, success: 'import started'
+  end
+
+  private
+  def import_csv_params
+    params.permit(:import_csv)
   end
 end
